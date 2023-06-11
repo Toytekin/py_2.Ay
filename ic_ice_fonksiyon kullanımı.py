615f52f25288fa4f5a8dac64d22f312bfda7e1b0
ibrahim <btoytekin80@gmail.com>
2023-06-11 21:38:54 +0300

    iç içe fonk kullanımı

---
+++ ic_ice_fonksiyon kullanımı.py
@@ -0,0 +1,29 @@
+def ana_fonksiyon(fonksiyon_adi):
+        
+        def toplama(*args):
+                toplam=0
+                for i in args:
+                        toplam+=i
+                print(toplam)
+        
+        def carpma(*args):
+                carpim=1
+                for i in args:
+                        carpim*=i
+                print(carpim)
+        
+        
+        if fonksiyon_adi=='toplama':
+                return toplama       
+        else:
+                return carpma
+        
+        
+toplama=ana_fonksiyon('toplama')
+carpma=ana_fonksiyon('carpma')
+
+toplama(1,2,3,4,5,6,7,8,9)
+carpma(1,2,3,4,5,6,7,8,9)
+
+
+            
